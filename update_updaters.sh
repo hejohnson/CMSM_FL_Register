6ac06a590a7746911dcbc22c65151f1f5beaf05f
Hans Johnson <hejohnson@wpi.edu>
2019-03-05 19:59:04 -0500

    Rexported jar

--- update_updaters.sh
+++ update_updaters.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 wget --backups=1 https://github.com/hejohnson/CMSM_FL_Register/raw/master/update_editor.sh
+chmod update_editor.sh
+
 wget --backups=1 https://github.com/hejohnson/CMSM_FL_Register/raw/master/update.sh
+chmod update.sh
