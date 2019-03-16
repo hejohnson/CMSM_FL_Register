6383a6491a495383d60a599a6b23e9dbd1606fe5
Hans Johnson <hejohnson@wpi.edu>
2019-03-16 11:01:04 -0400

    Fixed typo

--- update_editor.sh
+++ update_editor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "mv CashRegisterEditor_latest.jar CashRegisterEditor_old.jar;
 wget https://github.com/hejohnson/CMSM_FL_Register/raw/master/binaries/CashRegisterEditor_latest.jar;
-wget https://github.com/hejohnson/CMSM_FL_Regiser/raw/master/update_updaters.sh;
+wget --backups=1 https://github.com/hejohnson/CMSM_FL_Regiser/raw/master/update_updaters.sh;
 chmod +x update_updaters.sh" > temp.sh
 chmod +x temp.sh
 gnome-terminal -e temp.sh
