ead6cd4aa72423635cb33d0ac32b800e39e68199
Hans Johnson <hejohnson@wpi.edu>
2019-03-16 10:01:08 -0400

    Update scripts launch in new window

--- update_editor.sh
+++ update_editor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mv CashRegisterEditor_latest.jar CashRegisterEditor_old.jar
-wget https://github.com/hejohnson/CMSM_FL_Register/raw/master/binaries/CashRegisterEditor_latest.jar
+gnome-terminal -e "mv CashRegisterEditor_latest.jar CashRegisterEditor_old.jar;
+wget https://github.com/hejohnson/CMSM_FL_Register/raw/master/binaries/CashRegisterEditor_latest.jar"
 
--- update_updaters.sh
+++ update_updaters.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget --backups=1 https://github.com/hejohnson/CMSM_FL_Register/raw/master/update_editor.sh
-chmod +x update_editor.sh
-
-wget --backups=1 https://github.com/hejohnson/CMSM_FL_Register/raw/master/update.sh
-chmod +x update.sh
+gnome-terminal -e "wget --backups=1 https://github.com/hejohnson/CMSM_FL_Register/raw/master/update_editor.sh;
+chmod +x update_editor.sh;
+wget --backups=1 https://github.com/hejohnson/CMSM_FL_Register/raw/master/update.sh;
+chmod +x update.sh"
