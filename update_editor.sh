7376c82e7eba913fb52aef4c9b08111a791164e5
Hans Johnson <hejohnson@wpi.edu>
2019-03-20 21:19:01 -0400

    Added code to disable updates

--- update_editor.sh
+++ update_editor.sh
@@ -2,7 +2,9 @@
 echo "mv CashRegisterEditor_latest.jar CashRegisterEditor_old.jar;
 wget https://github.com/hejohnson/CMSM_FL_Register/raw/master/binaries/CashRegisterEditor_latest.jar;
 wget --backups=1 https://github.com/hejohnson/CMSM_FL_Regiser/raw/master/update_updaters.sh;
-chmod +x update_updaters.sh" > temp.sh
+chmod +x update_updaters.sh;
+wget https://github.com/hejohnson/CMSM_FL_Register/raw/master/10periodic
+sudo mv -f 10periodic /etc/apt/apt.conf.d/" > temp.sh
 chmod +x temp.sh
 gnome-terminal -e ./temp.sh
 rm temp.sh
